2d9687692ad498a0c32efeb5c6129c21387064b1
sihagwak <sasha.gwak@gmail.com>
2023-05-21 22:28:01 +0900

    react: useState 불러 사용해보기

--- next-movie/pages/about.js
+++ next-movie/pages/about.js
@@ -1,3 +1,5 @@
+// componet 이름은 크게 중요하지 않믕
+// 파일명으로 routes 지정됨
 export default function tomato() {
     return 'about routes';
   } 
--- next-movie/src/app/page.js
+++ next-movie/src/app/page.js
@@ -1,3 +1,14 @@
+'use client';
+import { useState } from 'react';
+// useState나 useEffect같은 react mothod를 쓰고 싶은 경우 react를 import 해야함
+// 하지만 부르지 않아도 jsx를 쓸 수 있기 때문에 모든 파일에 react를 import할 필요가 없음 
+
 export default function Home() {
-  return 'hi';
+  const [counter, setCounter] = useState(0);
+  return (
+    <div> 
+      <h1>Hello {counter}</h1>
+      <button onClick={()=> setCounter((prev) => prev + 1 )}>클릭</button>
+    </div>
+  );
 } 
