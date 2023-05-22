e4e6ccf855d6df36fcfea8f2354c0e85ad2f586c
sasha.gwak <sasha.gwak@gmail.com>
2023-05-22 12:24:13 +0900

    stduy: module.css로 여러가지 css 적용해보기

--- next-movie/components/NavBar.js
+++ next-movie/components/NavBar.js
@@ -6,14 +6,13 @@ import styles from './NavBar.module.css';
 export default function NavBar() {
     const pathname = usePathname();
     return (
-        // css modules 적용하기
-        // 실제 브라우저 class name 에선 NarBar_nav__0Bjy이런식으로 무작위로 생성되기때문에 충돌 발생하지 않음
-        // 다른곳에서도 nav라는 class명 쓸수 있는것 
-        <nav className={styles.nav}> 
-            <Link style={{color: pathname === "/" ? "red" : "blue" }} href="/">
+        <nav> 
+            {/* 문자열 만들어서 여러 css 먹이기 */}
+            <Link className={`${styles.link} ${pathname === "/" ? styles.active : "" }`} href="/">
                 Home
             </Link>
-            <Link style={{color: pathname === "/about"? "red" : "blue" }} href="/about">
+            {/* 배열 만들어서 여러 css 먹이기 */}
+            <Link className={[styles.link, pathname === "/about"? styles.active : "" ].join(' ')} href="/about">
                 About
             </Link>
         </nav>
