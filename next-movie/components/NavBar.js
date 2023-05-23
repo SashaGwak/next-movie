3006ff887ece62947135b8b446ceb9c28235df57
sasha.gwak <sasha.gwak@gmail.com>
2023-05-23 18:11:46 +0900

    study: fetch 사용하여 데이터 받기, redirect&rewrites 사용해보기

--- next-movie/components/NavBar.js
+++ next-movie/components/NavBar.js
@@ -8,15 +8,40 @@ export default function NavBar() {
     // 결국 스타일의 적용범위를 해당 컴포넌트 내부로 한정하는 것
     return (
         <nav> 
-            <Link href="/">
-                <span className={pathname === "/" ? "active" : ""}>Home</span>
-            </Link>
-            <Link href="/about">
-                <span className={pathname === "/about" ? "active" : ""}>About</span>
-            </Link>
-            <style jsx global>{`
+            <img src="/vercel.svg" />
+            <div>
+                <Link href="/">
+                    <span className={pathname === "/" ? "active" : ""}>Home</span>
+                </Link>
+                <Link href="/about">
+                    <span className={pathname === "/about" ? "active" : ""}>About</span>
+                </Link>
+            </div>
+            <style jsx>{`
+                nav {
+                    display: flex;
+                    gap: 10px;
+                    flex-direction: column;
+                    align-items: center;
+                    padding-top: 20px;
+                    padding-bottom: 10px;
+                    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+                        rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+                }
+                img {
+                    max-width: 100px;
+                    margin-bottom: 5px;
+                }
+                nav span {
+                    font-weight: 600;
+                    font-size: 18px;
+                }
                 .active {
-                    color : red;
+                    color: tomato;  
+                }
+                nav div {
+                    display: flex;
+                    gap: 10px;
                 }
             `}</style>
         </nav>
--- next-movie/next.config.js
+++ next-movie/next.config.js
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const API_KEY = process.env.API_KEY;
+
+const nextConfig = {
+    reactStrictMode: true, 
+    // Redirect를 통해 다른 페이지나 사이트로 이동할 수 있게 해줄 수있음 
+    // 아래 예시는 /contanct path로 들어오면 /form으로 이동하게 하는 것
+    async redirects() {
+        return [
+            {
+                source: '/contact',  // 들어오는 request 경로 패턴
+                destination: '/form',  // 라우팅하려는 경로(redirect할 경로)
+                permanent: false, 
+                // true 일경우 client와 search엔진에 redirect를 영구적으로 cache하도록 지시하는 308 code 사용 
+                // false 일경우 일시적이고 cache되지 않은 307 status code 사용
+            },
+            {
+                source: '/siha/:params', 
+                destination: `/hihi?key=${API_KEY}${encodeURIComponent("&")}:params`, // &처리 이렇게
+                permanent: false,
+            }// 이런식으로 계속 해서 써주면 됨
+        ]
+    }, 
+    // rewrites는 유저를 이동시키기는 하지만 Url은 변하지 않음(프록시 역할을 하며 destination 경로를 mask)
+    async rewrites() {
+        return [
+            {
+                source: '/api/movies', 
+                destination : `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
