de149c7a7f31f1ded5782022da7438f7ac6962d4
sasha.gwak <sasha.gwak@gmail.com>
2023-05-24 14:18:30 +0900

    study: detail 페이지이동(router, link 사용 두가지 방법) + url masking

--- next-movie/pages/index.js
+++ next-movie/pages/index.js
@@ -1,18 +1,42 @@
 import Seo from "../components/Seo";
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Home({results}) {
+    const router = useRouter(); 
+    // router를 이용하는 방법 1  
+    // const onClick = (id, title) => {
+    //     router.push(
+    //       {
+    //         pathname: `/movies/${id}`,
+    //         query: {
+    //           title,
+    //         },
+    //       },
+    //       `/movies/${id}`
+    //     );
+    //   };
     return (
         <div className="container">
             <Seo title="Home"/>
             {!results && <h4>Loading...</h4>}
             {results?.map((movie) => (
-                <Link href={`/movies/${movie.id}`} key={movie.id}> 
+                // <div onClick={() => onClick(movie.id, movie.original_title)}className="movie" key={movie.id}></div> 방법 1으로 요런식으로 덮어서 사용가능
+
+                // Link를 사용하는 방법 2
+                <Link href={{
+                        pathname: `/movies/${movie.id}`, 
+                        query : {
+                            title : movie.original_title
+                        }, 
+                    }} 
+                    as={`/movies/${movie.id}`}>  
+                    {/* as를 통해 query masking 처리 가능 */}
                     <div className="movie">
                         <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
                         <h4>{movie.original_title}</h4>
                     </div>
-                </Link>
+                </Link>   
             ))}
             <style jsx>{`
                 .container {
@@ -42,15 +66,12 @@ export default function Home({results}) {
     );
 }
 
-// server 선행
 export async function getServerSideProps() {
 
     const { results } = await(await fetch(`http://localhost:3000/api/movies`)).json();
     return {
         props: {
-            results, // 요 result를 위에서 받아줌
-            // 무엇을 return하던지 props로서 Page에게 주게됨 
-            // _app.js에서 사용하는 pageProps에서 받는 정보 
+            results,
         }
     }
 }
