c4b36d7283c62ea8c11ca1dedb1f7ad42d3d7ab6
sasha.gwak <sasha.gwak@gmail.com>
2023-05-24 16:59:06 +0900

    study: detail page SSR

--- next-movie/pages/movies/[...params].js
+++ next-movie/pages/movies/[...params].js
@@ -1,13 +1,22 @@
 import { useRouter } from "next/router";
+import Seo from "../../components/Seo";
 
-export default function Detail() {
-    const router = useRouter();
-    const [title, id] = router.query.params || [];
-    // SSR을 하지 않는 경우 pre-rendering 되기 때문에 useRouter를 제대로 가져오지 못함(선 html 후 js)
-    // 그래서 초기값으로 빈배열을 줘서 오류 발생 막고, js 가져온 뒤 재랜더링 하면 router를 제대로 가져오는것
+export default function Detail({params}) {
+    const [title, id] = params || [];
     return (
         <div> 
+            <Seo title={title}/>
             <h4>{title}</h4>
         </div>
     );
+}
+
+// api 요청하지 않고도 이렇게 SSR하고있어서 속도도 빠르고 SEO측면에서 장점 가질 수 있음 ! 
+export function getServerSideProps({params: {params}}) {
+    console.log(params); // [ title, id ]
+    return {
+        props:{
+            params
+        }, 
+    }
 }
